c58f01ebadeb59a7eac1a9afe15871fc9013c878
unknown <german.f.pereyra@gmail.com>
2015-03-01 08:08:01 -0300

    addDBParamtetersFromConsult method added

--- App_Code/entities/Consult.cs
+++ App_Code/entities/Consult.cs
@@ -47,4 +47,64 @@ public class Consult
     public string convertToJson () {
         return "{\"start\":  \"" + startDate.ToString("yyyy-MM-ddTHH:mm:ss") + "\", \"end\":  \"" + this.endDate.ToString("yyyy-MM-ddTHH:mm:ss") + "\", \"title\":\"" + this.patient.name + "\", \"id\":\"" + this.id + "\"}";
     }
+
+    public static void addDBParametersFromConsult(Consult c, SqlCommand comm)
+    {
+        SqlParameter startDate = new SqlParameter("@startDate", System.Data.SqlDbType.DateTime);
+        startDate.Value = c.startDate;
+        comm.Parameters.Add(startDate);
+
+        SqlParameter endDate = new SqlParameter("@endDate", System.Data.SqlDbType.DateTime);
+        endDate.Value = c.endDate;
+        comm.Parameters.Add(endDate);
+
+        SqlParameter price = new SqlParameter("@price", System.Data.SqlDbType.Int);
+        price.Value = c.price;
+        comm.Parameters.Add(price);
+
+        SqlParameter patient = new SqlParameter("@patientId", System.Data.SqlDbType.Int);
+        patient.Value = c.patient;
+        comm.Parameters.Add(patient);
+
+        SqlParameter diagnostic = new SqlParameter("@diagnostic", System.Data.SqlDbType.Text);
+        diagnostic.Value = c.diagnostic;
+        comm.Parameters.Add(diagnostic);
+
+        if (c.treatment != null)
+        {
+            SqlParameter treatment = new SqlParameter("@treatment", System.Data.SqlDbType.Text);
+            treatment.Value = c.treatment;
+            comm.Parameters.Add(treatment);
+        }
+
+        if (c.propousal != null)
+        {
+            SqlParameter propousal = new SqlParameter("@propousal", System.Data.SqlDbType.Text);
+            propousal.Value = c.propousal;
+            comm.Parameters.Add(propousal);
+        }
+
+        if (c.clinicalAnalysis != null)
+        {
+            SqlParameter clinicalAnalysis = new SqlParameter("@clinicalAnalysis", System.Data.SqlDbType.Text);
+            clinicalAnalysis.Value = c.clinicalAnalysis;
+            comm.Parameters.Add(clinicalAnalysis);
+        }
+
+        if (c.rating > 0)
+        {
+            SqlParameter rating = new SqlParameter("@rating", System.Data.SqlDbType.Int);
+            rating.Value = c.rating;
+            comm.Parameters.Add(rating);
+        }
+        if (c.assignedTo != null)
+        {
+            SqlParameter assignedTo = new SqlParameter("@assignedTo", System.Data.SqlDbType.Int);
+            assignedTo.Value = c.assignedTo.id;
+            comm.Parameters.Add(assignedTo);
+        }
+
+
+    }
+    
 }
